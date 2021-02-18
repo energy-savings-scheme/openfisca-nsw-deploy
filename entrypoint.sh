9277755f378014ab7e509ae1cff4b19eaf182830
Liam McCann <fromcassettes@gmail.com>
2021-02-18 11:11:44 +1100

    remove unneeded extensions switch in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/local/bin/openfisca serve --workers=4 --country-package openfisca_nsw_base --extensions openfisca_nsw_ess_nabers --extensions openfisca_nsw_ess_heer --bind 0.0.0.0:$PORT
+/usr/local/bin/openfisca serve --workers=4 --country-package openfisca_nsw_base --extensions openfisca_nsw_ess_nabers openfisca_nsw_ess_heer --bind 0.0.0.0:$PORT
