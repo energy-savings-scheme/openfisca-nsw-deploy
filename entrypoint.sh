6e7e40774c2446c4a782d34e5174b49578b5122e
Liam McCann <fromcassettes@gmail.com>
2021-04-13 09:26:48 +1000

    remove openfisca_nsw_heer from .gitmodules, Dockerfile, entrypoint, .git/config

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/local/bin/openfisca serve --workers=3 --country-package openfisca_nsw_base --extensions openfisca_nsw_ess_heer openfisca_nsw_pdrs --bind 0.0.0.0:$PORT
+/usr/local/bin/openfisca serve --workers=2 --country-package openfisca_nsw_base --extensions openfisca_nsw_pdrs --bind 0.0.0.0:$PORT
